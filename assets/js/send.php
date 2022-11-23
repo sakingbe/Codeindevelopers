362641f91fdfd48f35595d72a02d0db51b6f4bb5
JesuZboi <60920246+JesuZboi@users.noreply.github.com>
2022-11-24 00:40:38 +0100

    gdgttu

--- assets/js/send.php
+++ assets/js/send.php
@@ -1,1 +1,36 @@
-{"success":false,"message":"Name is required."}
+<?php
+$name = $_POST["fullname"];
+$email = $_POST["email"];
+$subject = $_POST["subject"];
+$mobilenumber = $_POST["mobilenumber"];
+$interestedin = $_POST["interestedin"];
+$description = $_POST["description"];
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+$mail = new PHPMailer(true);
+
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+
+$mail->isSMTP();
+$mail->SMTPAuth = true;
+
+$mail->Host = "smtp-relay.sendinblue.com";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->Port = 587;
+
+$mail->Username = "samuelmayowa33@gmail.com";
+$mail->Password = "MVxyGW3smqtawTEQ";
+
+$mail->setFrom($email, $name);
+$mail->addAddress("samuelmayowa33@gmail.com", "Samuel");
+
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+$mail->send();
+
+header("Location: sent.html");
