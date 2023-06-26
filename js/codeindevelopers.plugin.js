0cc6965ceeefdeaeabe6a873c5b5c95962c8dcb9
JesuZboi <60920246+JesuZboi@users.noreply.github.com>
2023-06-26 12:34:55 +0100

    sitemap

--- js/codeindevelopers.plugin.js
+++ js/codeindevelopers.plugin.js
@@ -6181,3 +6181,27 @@ menuSection.addEventListener("click", (e) => {
     }),
     (a.scrollUp = a.fn.scrollUp);
 })(jQuery, window, document);
+const cookieBox = document.querySelector(".wrapper"),
+  buttons = document.querySelectorAll(".button");
+
+const executeCodes = () => {
+  //if cookie contains Codeindevelopers it will be returned and below of this code will not run
+  if (document.cookie.includes("Codeindevelopers")) return;
+  cookieBox.classList.add("show");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      cookieBox.classList.remove("show");
+
+      //if button has acceptBtn id
+      if (button.id == "acceptBtn") {
+        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+        document.cookie =
+          "cookieBy= Codeindevelopers; max-age=" + 60 * 60 * 24 * 30;
+      }
+    });
+  });
+};
+
+//executeCodes function will be called on webpage load
+window.addEventListener("load", executeCodes);
